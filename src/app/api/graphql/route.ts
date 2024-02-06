27b57588ae4a3e54406bf722818c7a97e1eaeba7
SoraKumo <info@croud.jp>
2024-02-06 14:00:57 +0900

    GraphQLルートの変更を追加

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -4,18 +4,25 @@ import { Context, prisma } from './libs/context';
 import { schema } from './libs/schema';
 import { getUserFromToken } from '../../../libs/getUserFromToken';
 
-const { handleRequest } = createYoga<Context>({
-  schema: schema(),
-  fetchAPI: { Response },
-  context: async ({ request: req }) => {
-    console.log('context');
-    const cookies = getCookies();
-    const token = cookies.get('auth-token')?.value;
-    const user = await getUserFromToken(token);
-    return { req, prisma, user, cookies };
-  },
-});
+let handleRequest: (request: Request, ctx: Context) => Response | Promise<Response>;
 
-export { handleRequest as GET, handleRequest as POST, handleRequest as OPTIONS };
+const callRequest = (request: Request, ctx: Context) => {
+  if (!handleRequest) {
+    handleRequest = createYoga<Context>({
+      schema: schema(),
+      fetchAPI: { Response },
+      context: async ({ request: req }) => {
+        console.log('context');
+        const cookies = getCookies();
+        const token = cookies.get('auth-token')?.value;
+        const user = await getUserFromToken(token);
+        return { req, prisma, user, cookies };
+      },
+    }).handleRequest;
+  }
+  return handleRequest(request, ctx);
+};
+
+export { callRequest as GET, callRequest as POST, callRequest as OPTIONS };
 
 export const runtime = 'edge';
